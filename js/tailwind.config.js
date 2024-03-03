7c790f71fafcdbc48a2127d4630de25bbd2c1b84
unknown <abusiddick84@gmail.com>
2024-03-03 13:13:49 +0600

    Banner area with responsive

---
+++ js/tailwind.config.js
@@ -0,0 +1,10 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                panton: '#797DFC',
+                navyBlue: '#12132D'
+            }
+        }
+    }
+}
